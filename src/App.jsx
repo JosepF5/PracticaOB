d7a7e8d251b1f504ce654f0e4a302b4ef196c597
Josep Palomino <jfpalomino.22@gmail.com>
2022-07-31 12:11:57 -0500

    Entrega ejercicios 10, 11 y 12

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import ContactList from './components/container/ContactList'
 import Clock from './components/container/Clock'
+import Rectangle from './components/container/Rectangle'
 function App() {
 
   return (
     <div className="App">
       <Clock />
       <ContactList />
+      <Rectangle />
     </div>
   )
 }
